be7cf50794bb4aeec2623baf03d231575b5f9d2e
AbirHossainNishat <abirnishat66@gmail.com>
2025-11-09 23:51:38 +0600

    my second repo

--- obj.js
+++ obj.js
@@ -1,7 +1,18 @@
-const colors = {
-    red: "#ff0000",
-    green: "#00ff00",
-    blue: "#0000ff",
-    "golden rod": '#daa520'
+// const colors = {
+//     red: "#ff0000",
+//     green: "#00ff00",
+//     blue: "#0000ff",
+//     "golden rod": '#daa520'
+// };
+// // console.log(colors ["golden rod"]);
+
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020,
+   
+passenger : 5
+
+
 };
-console.log(colors ["golden rod"]);
+console.log(car)
